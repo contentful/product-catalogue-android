307e6a81941893e38f221f152a4cea512a722dd0
Tom Reznik <xoreznik@gmail.com>
2015-07-23 11:01:38 +0200

    Vault v0.9.7

--- app/src/main/java/catalogue/templates/contentful/lib/ClientProvider.java
+++ app/src/main/java/catalogue/templates/contentful/lib/ClientProvider.java
@@ -23,9 +23,8 @@ public class ClientProvider {
         String accessToken = defaultIfBlank(Preferences.getAccessToken(), Const.ACCESS_TOKEN);
 
         instance = CDAClient.builder()
-            .setSpaceKey(spaceId)
-            .setAccessToken(accessToken)
-            .nullifyUnresolvedLinks()
+            .setSpace(spaceId)
+            .setToken(accessToken)
             .build();
       }
 
